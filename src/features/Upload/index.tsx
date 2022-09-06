1e51b7e893f0eea77f48df51f16b2a55dc5268f9
SeptAlfauzan <alfauzansepta@gmail.com>
2022-09-06 11:02:08 +0700

    fix upload page layout

--- src/features/Upload/index.tsx
+++ src/features/Upload/index.tsx
@@ -69,9 +69,9 @@ const Upload: React.FC = () => {
           display={"flex"}
           width={"full"}
           marginBottom={70}
-          flexWrap={"wrap"}
           alignItems={"start"}
-          justifyContent={"center"}
+          flexDirection={"column"}
+          gap={10}
         >
           <Text
             width={"100%"}
@@ -81,91 +81,95 @@ const Upload: React.FC = () => {
           >
             Incolla
           </Text>
-          <Box
-            flexGrow={1}
-            width={{ base: "full", md: "50%" }}
-            display={"flex"}
-            justifyContent={{ base: "center", lg: "start" }}
-            flexWrap={"wrap"}
-            gap="10px"
-          >
-            <Box width={"full"} marginBottom={8}>
-              <Text textAlign={"left"} color={"blackAlpha.700"}>
-                Easy way to generate bunch sertificates, only need the names
-                list and the certificate's image
-              </Text>
-              <Text
-                display={{ base: "block", md: "none" }}
-                fontSize={12}
-                color={"red.200"}
+          <Box display={"flex"} flexDirection={{ base: "column", md: "row" }}>
+            <Box
+              flexGrow={1}
+              width={{ base: "full", md: "50%" }}
+              display={"flex"}
+              justifyContent={{ base: "center", lg: "start" }}
+              flexWrap={"wrap"}
+              gap="10px"
+            >
+              <Box width={"full"} marginBottom={8}>
+                <Text textAlign={"left"} color={"blackAlpha.700"}>
+                  Easy way to generate bunch sertificates, only need the names
+                  list and the certificate's image
+                </Text>
+                <Text
+                  display={{ base: "block", md: "none" }}
+                  fontSize={12}
+                  color={"red.200"}
+                >
+                  You can see the preview by scrolling this page
+                </Text>
+              </Box>
+              <DragDropInput
+                onAccept={onAcceptImageFile}
+                result={imageData}
+                acceptedFilesProps={{ "image/png": [".png", ".jpg", ".jpeg"] }}
+                isAccepted={setImageaccept}
               >
-                You can see the preview by scrolling this page
-              </Text>
+                <FcImageFile size={60} />
+                <Text color={"blue.400"} textAlign={"center"} fontSize={14}>
+                  Drag your certificate's image template
+                </Text>
+              </DragDropInput>
+              <DragDropInput
+                onAccept={onAcceptCSVFile}
+                result={csvData}
+                acceptedFilesProps={{ "text/csv": [".csv"] }}
+                isAccepted={setCsvaccept}
+              >
+                <FcFile size={60} />
+                <Text color={"blue.400"} textAlign={"center"} fontSize={14}>
+                  Drag your .csv file
+                </Text>
+              </DragDropInput>
+              <Button
+                width={{ base: "full", lg: "130px" }}
+                disabled={
+                  imageData !== "" && csvData.length !== 0 ? false : true
+                }
+                color={
+                  imageData !== "" && csvData.length !== 0
+                    ? "whiteAlpha.800"
+                    : "black"
+                }
+                _hover={{
+                  color: "white",
+                  bgGradient: "linear(to-bl, #7928CA, #FF0080 50%)",
+                  transition: "all 100ms linear",
+                }}
+                bgGradient={
+                  imageData !== "" && csvData.length !== 0
+                    ? "linear(to-l, #7928CA, #FF0080)"
+                    : "ButtonFace"
+                }
+                position={{ base: "relative", md: "relative" }}
+                bottom={0}
+                alignSelf={{ base: "center", md: "flex-start" }}
+                onClick={() => navigate("/customize")}
+                marginTop={"10px"}
+                display={"block"}
+              >
+                Next Step
+              </Button>
             </Box>
-            <DragDropInput
-              onAccept={onAcceptImageFile}
-              result={imageData}
-              acceptedFilesProps={{ "image/png": [".png", ".jpg", ".jpeg"] }}
-              isAccepted={setImageaccept}
+            <Box
+              backgroundColor={"twitter.50"}
+              borderRadius={15}
+              alignSelf={"flex-start"}
+              justifySelf={"flex-end"}
+              marginLeft={{ base: 0, lg: "50px" }}
+              marginTop={{ base: "20px", lg: 0 }}
+              width={{ base: "100%", lg: "300px" }}
             >
-              <FcImageFile size={60} />
-              <Text color={"blue.400"} textAlign={"center"} fontSize={14}>
-                Drag your certificate's image template
-              </Text>
-            </DragDropInput>
-            <DragDropInput
-              onAccept={onAcceptCSVFile}
-              result={csvData}
-              acceptedFilesProps={{ "text/csv": [".csv"] }}
-              isAccepted={setCsvaccept}
-            >
-              <FcFile size={60} />
-              <Text color={"blue.400"} textAlign={"center"} fontSize={14}>
-                Drag your .csv file
-              </Text>
-            </DragDropInput>
-            <Button
-              width={{ base: "full", lg: "130px" }}
-              disabled={imageData !== "" && csvData.length !== 0 ? false : true}
-              color={
-                imageData !== "" && csvData.length !== 0
-                  ? "whiteAlpha.800"
-                  : "black"
-              }
-              _hover={{
-                color: "white",
-                bgGradient: "linear(to-bl, #7928CA, #FF0080 50%)",
-                transition: "all 100ms linear",
-              }}
-              bgGradient={
-                imageData !== "" && csvData.length !== 0
-                  ? "linear(to-l, #7928CA, #FF0080)"
-                  : "ButtonFace"
-              }
-              position={{ base: "relative", md: "relative" }}
-              bottom={0}
-              alignSelf={{ base: "center", md: "flex-start" }}
-              onClick={() => navigate("/customize")}
-              marginTop={"10px"}
-              display={"block"}
-            >
-              Next Step
-            </Button>
-          </Box>
-          <Box
-            backgroundColor={"twitter.50"}
-            borderRadius={15}
-            alignSelf={"flex-start"}
-            justifySelf={"flex-end"}
-            marginLeft={{ base: 0, lg: "50px" }}
-            marginTop={{ base: "20px", lg: 0 }}
-            width={{ base: "100%", lg: "300px" }}
-          >
-            <Preview
-              ref={previewAccordion}
-              imageURL={imageData}
-              nameList={csvData}
-            />
+              <Preview
+                ref={previewAccordion}
+                imageURL={imageData}
+                nameList={csvData}
+              />
+            </Box>
           </Box>
         </Box>
         <HowToUse />
